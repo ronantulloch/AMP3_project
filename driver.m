64e6e6552da6c58d5bd1a8e08d2459830f9e42b0
Ronan Tulloch <Ronantulloch@gmail.com>
2022-08-29 14:45:13 +0930

    29/08

--- driver.m
+++ driver.m
@@ -5,18 +5,17 @@ if exist(dfile, 'file') ; delete(dfile); end
 diary(dfile)
 diary on
 
+% %Clean the chosen file.
+% executed = system("R CMD BATCH manipulating.R"); %Try not to run too often as this script takes too long.
 
-
-%Clean the chosen file.
-executed = system('R CMD BATCH manipulating.R'); %Try not to run too often as this script takes too long.
-
-Event_Log =  ["a", "b", "B", "Bb"]; %Later turn this into READ CSV.
+Event_Log =  ["car", "cab", "dar", "dab"]; %Later turn this into READ CSV.
 
 %Get the DFA from the event log.
-[Q, Sigma, delta, q_0, F] = DFA_construct(Event_Log)
+[Q, Sigma, delta, q_0, F] = DFA_construct(Event_Log);
 
 %Get the transition matrix from the DFA.
-P = DFA_to_markov(delta, Event_Log)
+P = DFA_to_markov(delta, Event_Log);
 
 %Minimised the DFA
+[Q, Sigma, delta, q_0, F] = transform(Q, Sigma, delta, F); %Transform 
 diary off
--- transform.m
+++ transform.m
@@ -1,3 +1,17 @@
-function [Q, Sigma, delta, q_0, F] = transform(Q, Sigma, delta, q_0, F)
-%%Transform output from algorithm 1 to algorithm 2.
+function [Q, Sigma, delta, q_0, F] = transform(Q, Sigma, delta, F)
+%Transform Q
+Q = double(Q(1,:)) + 1
+
+%Transform q_0
+q_0 = (double(delta(delta(:,2) == "", 4))') + 1
+
+%Transform F
+F = double(F(1,:)) + 1
+
+%Transform delta
+
+
+
+%Transform delta
+
 end
