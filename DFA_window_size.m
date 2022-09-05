ab9de98d6d10e964ad8797f2cba637e4dfe1db17
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-05 15:59:51 +0930

    Update DFA_window_size.m
    Fixed the windowed DFA construction

--- DFA_window_size.m
+++ DFA_window_size.m
@@ -1,23 +1,25 @@
-function M = DFA_window_size(M, k)
+function M = DFA_window_size(A, k)
 %This function transforms the states of a DFA to be given a set window size.
 
-%Grab the transition array.
-delta = M{3};
+%Initialise the windows of DFA elements.
+windowed_A = [];
 
-for i = 1:size(delta, 1)
-	%Get the state identifiers as character arrays
-	first_state = char(delta(i,2));
-	second_state = char(delta(i, 5));
+for i = 1:length(A)
+	%Grab the current element to window.
+	current_element = char(A(i));
 
-	%If the state identifier is long enough take the window and place back into delta.
-	if ~(length(first_state) <= k)
-		new_first_state = string(first_state((end-k+1):end));
-		new_second_state = string(second_state((end-k+1):end));
-		delta(i,2) = new_first_state;
-		delta(i,5) = new_second_state;
+	%Make the prefixes.
+	for j = 1:length(current_element)
+		if length(current_element(1:j)) < k
+			%Grab the firs few prefixes that are not k length.
+			windowed_A = [windowed_A, string(current_element(1:j))];
+		else
+			%Grab the rest of the windows.
+			windowed_A = [windowed_A, string(current_element(j-k+1:j))];
+		end
 	end
 end
 
-%Place the new delta back into the model.
-M{3} = delta;
+%Grab the DFA.
+M = DFA_construct(windowed_A);
 end
