7f9126e3100abfcbe02160da229eb7f90b61bcb6
ben-osborne1 <102773171+ben-osborne1@users.noreply.github.com>
2022-09-06 17:00:45 +0930

    Add files via upload

--- precall_irr.m
+++ precall_irr.m
@@ -8,13 +8,6 @@ function [p,r] = precall_irr(M, L)
 if size(M{1},1) ~= 1 % convert to simple form if needed
     M = convert_simple(M);
 end
-F = M{5};
-
-Q_list = state_list(M);
-F_list = [];
-for i = 1:length(F)
-    F_list = [F_list, Q_list{F(i)}];
-end
 
 ML = DFA_construct(L); ML = minimize_DFA(ML);
 MMnL = cross_product(M, ML);
