02a4f94e9cf27ea6ed89aefdedc7714beb3aafc9
ben-osborne1 <102773171+ben-osborne1@users.noreply.github.com>
2022-09-06 15:31:16 +0930

    Add files via upload

--- precall.m
+++ precall.m
@@ -3,12 +3,12 @@ function [p,r] = precall(M, L)
 %   M: A DFA (cell array)
 %   L: An event log (string array)
 % Output:
-%   The precision of the model M on the log L
+%   The precision & recall of the model M on the log L
 
 if size(M{1},1) ~= 1 % convert to simple form if needed
     M = convert_simple(M);
 end
-Q = M{1}; sigma = M{2}; delta = M{3}; q_0 = M{4}; F = M{5};
+F = M{5};
 
 Q_list = state_list(M);
 F_list = [];
