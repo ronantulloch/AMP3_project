c713084955886cb2298178a8752a9c730efbf9f4
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-06 20:33:57 +0930

    Update DFA_window_size.m

--- DFA_window_size.m
+++ DFA_window_size.m
@@ -1,25 +1,23 @@
-function M = DFA_window_size(A, k)
+function M = DFA_window_size(M, k)
 %This function transforms the states of a DFA to be given a set window size.
 
-%Initialise the windows of DFA elements.
-windowed_A = [];
+%Grab the transition array.
+delta = M{3};
 
-for i = 1:length(A)
-	%Grab the current element to window.
-	current_element = char(A(i));
+for i = 1:size(delta, 1)
+	%Get the state identifiers as character arrays
+	first_state = char(delta(i,2));
+	second_state = char(delta(i, 5));
 
-	%Make the prefixes.
-	for j = 1:length(current_element)
-		if length(current_element(1:j)) < k
-			%Grab the firs few prefixes that are not k length.
-			windowed_A = [windowed_A, string(current_element(1:j))];
-		else
-			%Grab the rest of the windows.
-			windowed_A = [windowed_A, string(current_element(j-k+1:j))];
-		end
+	%If the state identifier is long enough take the window and place back into delta.
+	if ~(length(first_state) <= k)
+		new_first_state = string(first_state((end-k+1):end));
+		new_second_state = string(second_state((end-k+1):end));
+		delta(i,2) = new_first_state;
+		delta(i,5) = new_second_state
 	end
 end
 
-%Grab the DFA.
-M = DFA_construct(windowed_A);
+%Place the new delta back into the model.
+M{3} = delta;
 end
