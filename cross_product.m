07954b0dc31481e89b871858f429de370a5d46b2
ben-osborne1 <102773171+ben-osborne1@users.noreply.github.com>
2022-09-06 15:36:16 +0930

    Add files via upload
    Modified cross_product() to accept any DFAs, not just prefix trees. Note it now has a dependency on state_list()

--- cross_product.m
+++ cross_product.m
@@ -1,18 +1,29 @@
 function [M3] = cross_product(M1, M2)
 % Inputs:
-%   M1 of the form {Q, Sigma, delta, q_0, F} Expanded Form **
-%   M2 of the form {Q, Sigma, delta, q_0, F} Expanded Form **
+%   Two DFAs M1 & M2
 % Output:
 %   M3 = {Q3, Sigma, delta3, q_3, F3} Expanded Form **
 
-% test inputs
-if size(M1{5}, 1) ~= 2
-    disp("Wrong format, please input an expanded form DFA")
-    return
+if size(M1{1},1) ~= 1 % convert to simple form if needed
+    M1 = convert_simple(M1);
+end
+if size(M2{1},1) ~= 1 % convert to simple form if needed
+    M2 = convert_simple(M2);
+end
+F1 = M1{5}; F2 = M2{5};
+
+Q_list1 = state_list(M1);
+F_list1 = [];
+for i = 1:length(F1)
+    F_list1 = [F_list1, Q_list1{F1(i)}];
+end
+Q_list2 = state_list(M2);
+F_list2 = [];
+for i = 1:length(F2)
+    F_list2 = [F_list2, Q_list2{F2(i)}];
 end
 
-F1 = M1{5}; F2 = M2{5};
-F3 = intersect(F1(2,:), F2(2,:)); 
-M3 = DFA_construct(F3);
-
+F_list = intersect(F_list1,F_list2);
+M3 = DFA_construct(F_list);
+M3 = convert_simple(M3);
 end
