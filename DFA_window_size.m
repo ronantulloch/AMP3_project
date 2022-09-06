48488641f0852b623c45a41efda95f710e0c4bca
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-06 20:44:05 +0930

    Update DFA_window_size.m

--- DFA_window_size.m
+++ DFA_window_size.m
@@ -10,7 +10,7 @@ for i = 1:size(delta, 1)
 	second_state = char(delta(i, 5));
 
 	%If the state identifier is long enough take the window and place back into delta.
-	if ~(length(first_state) <= k)
+	if ~(length(first_state) < k)
 		new_first_state = string(first_state((end-k+1):end));
 		new_second_state = string(second_state((end-k+1):end));
 		delta(i,2) = new_first_state;
@@ -19,5 +19,5 @@ for i = 1:size(delta, 1)
 end
 
 %Place the new delta back into the model.
-M{3} = delta;
+M{3} = unique(delta, 'rows');
 end
