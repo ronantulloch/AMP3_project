7003301b7eba6d5d6157fbe872bce1590237b988
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-09 08:39:01 +0930

    Create DFA_vis.m
    Not working yet, but building a script that builds DFA visualisation.

---
+++ DFA_vis.m
@@ -0,0 +1,6 @@
+function [] = DFA_vis(M)
+%%Input a DFA in the form of M = {Q, Sigma, delta, q_0, F} and this function will create a
+%%visualisation of the graph that relates the states.
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
