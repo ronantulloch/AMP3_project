a23b7a6637ded8931e580a704afaf3716e1c9063
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-09 12:40:01 +0930

    Update DFA_vis.m

--- DFA_vis.m
+++ DFA_vis.m
@@ -5,11 +5,18 @@ function [] = DFA_vis(M)
 %Grab the transition function and states and initialise the graph.
 delta = M{3};
 Q = M{1}; Q = Q(2,:);
+F = M{5}; F = F(2,:);
+final_index = [];
 G = digraph;
 
 %Add the nodes to the graph
 for i = 1:length(Q)
 	G = addnode(G, Q(i));
+	
+	%Check if final state.
+	if any(Q(i) == F)
+		final_index = [final_index, i];
+	end
 end
 
 
@@ -18,7 +25,12 @@ for i = 1:size(delta,1)
 	G = addedge(G, delta(i,2), delta(i,5));
 end
 
+%Add the names to the transitions
+G.Edges.names = delta(:,3);
+
 
 %Plot the graph
-plot(G);
+h = plot(G,'Layout','force', "Marker", "o");
+h.EdgeLabel = G.Edges.names;
+highlight(h, final_index); %Highlight the final states.
 end
