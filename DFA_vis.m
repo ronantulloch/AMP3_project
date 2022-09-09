af1b9c2ab822dedc30c6a6e1330cf64eb4298821
Ronan Tulloch <Ronantulloch@gmail.com>
2022-09-09 11:17:57 +0930

    Update DFA_vis.m

--- DFA_vis.m
+++ DFA_vis.m
@@ -1,6 +1,24 @@
 function [] = DFA_vis(M)
 %%Input a DFA in the form of M = {Q, Sigma, delta, q_0, F} and this function will create a
 %%visualisation of the graph that relates the states.
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+%Grab the transition function and states and initialise the graph.
+delta = M{3};
+Q = M{1}; Q = Q(2,:);
+G = digraph;
+
+%Add the nodes to the graph
+for i = 1:length(Q)
+	G = addnode(G, Q(i));
+end
+
+
+%Add the transitions to the graph
+for i = 1:size(delta,1)
+	G = addedge(G, delta(i,2), delta(i,5));
+end
+
+
+%Plot the graph
+plot(G);
 end
